be2ef7a4921ae9bf8bc807b29592b6e8d8c56c02
Katell Goaer <alt.katellgoaer@gmail.com>
2020-02-21 16:56:22 -0500

    added cloudinary picture system

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 require "json"
 require "open-uri"
 
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
 
 
 url_ingredients = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
@@ -25,31 +25,31 @@ ingredients.each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
 end
 
-mint_julep = Cocktail.create(name: "Mint Julep")
-whiskey_sour = Cocktail.create(name: "Whiskey Sour")
-mojito = Cocktail.create(name: "Mojito")
+# mint_julep = Cocktail.create(name: "Mint Julep")
+# whiskey_sour = Cocktail.create(name: "Whiskey Sour")
+# mojito = Cocktail.create(name: "Mojito")
 
 
-Dose.create(description: '1/3 cup',
-            cocktail: mojito,
-            ingredient: Ingredient.where(name: 'Sugar').first)
+# Dose.create(description: '1/3 cup',
+#             cocktail: mojito,
+#             ingredient: Ingredient.where(name: 'Sugar').first)
 
-Dose.create(description: '1/3 cup',
-            cocktail: mojito,
-            ingredient: Ingredient.where(name: 'Light rum').first)
+# Dose.create(description: '1/3 cup',
+#             cocktail: mojito,
+#             ingredient: Ingredient.where(name: 'Light rum').first)
 
-Dose.create(description: '1/3 cup',
-            cocktail: whiskey_sour,
-            ingredient: Ingredient.where(name: 'Apricot brandy').first)
+# Dose.create(description: '1/3 cup',
+#             cocktail: whiskey_sour,
+#             ingredient: Ingredient.where(name: 'Apricot brandy').first)
 
-Dose.create(description: '1/3 cup',
-            cocktail: whiskey_sour,
-            ingredient: Ingredient.where(name: 'Triple sec').first)
+# Dose.create(description: '1/3 cup',
+#             cocktail: whiskey_sour,
+#             ingredient: Ingredient.where(name: 'Triple sec').first)
 
-Dose.create(description: '1/3 cup',
-            cocktail: mint_julep,
-            ingredient: Ingredient.where(name: 'Bourbon').first)
+# Dose.create(description: '1/3 cup',
+#             cocktail: mint_julep,
+#             ingredient: Ingredient.where(name: 'Bourbon').first)
 
-Dose.create(description: '1/3 cup',
-            cocktail: mint_julep,
-            ingredient: Ingredient.where(name: 'Grenadine').first)
+# Dose.create(description: '1/3 cup',
+#             cocktail: mint_julep,
+#             ingredient: Ingredient.where(name: 'Grenadine').first)
